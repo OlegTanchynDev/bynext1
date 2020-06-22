220ddf05d6d4517f8c7f6c16928d658d0e9aa20e
andrew <andrew_mokryj@ukr.net>
2020-06-22 20:47:19 +0300

    Throw RestError on login with wrong data 2

--- lib/model/rest_error.dart
+++ lib/model/rest_error.dart
@@ -1,31 +1,40 @@
 class RestError implements Exception {
-  const RestError({this.username, this.password, this.nonFieldErrors,});
 
-  final List username;
-  final List password;
-  final List nonFieldErrors;
+  const RestError({this.nonFieldErrors, this.fieldErrors,});
 
+  final String nonFieldErrors;
+  final Map<String, String> fieldErrors;
+
+  static const String _keyFieldErrors = 'fieldErrors';
   static const String _keyUsername = 'username';
   static const String _keyPassword = 'password';
   static const String _keyNonFieldErrors = 'non_field_errors';
 
   factory RestError.fromJson(Map<String, dynamic> map) {
+    final fieldMap = {
+      _keyUsername: (map[_keyUsername] as List).join(", "),
+      _keyPassword: (map[_keyPassword] as List).join(", "),
+    };
     return RestError(
-      username: map[_keyUsername],
-      password: map[_keyPassword],
-      nonFieldErrors: map[_keyNonFieldErrors],
+      nonFieldErrors: (map[_keyNonFieldErrors] as List).join(", "),
+      fieldErrors: fieldMap,
     );
   }
 
   Map<String, dynamic> toMap() => <String, dynamic>{
-    _keyUsername: username,
-    _keyPassword: password,
     _keyNonFieldErrors: nonFieldErrors,
+    _keyUsername: fieldErrors[_keyUsername],
+    _keyPassword: fieldErrors[_keyPassword],
   };
 
-  factory RestError.fromMap(Map<String, dynamic> map) => RestError(
-    username: map[_keyUsername],
-    password: map[_keyPassword],
-    nonFieldErrors: map[_keyNonFieldErrors],
-  );
+  factory RestError.fromMap(Map<String, dynamic> map) {
+    final fieldMap = {
+      _keyUsername: (map[_keyUsername] as List).join(", "),
+      _keyPassword: (map[_keyPassword] as List).join(", "),
+    };
+    return RestError(
+      nonFieldErrors: (map[_keyNonFieldErrors] as List).join(", "),
+      fieldErrors: fieldMap,
+    );
+  }
 }

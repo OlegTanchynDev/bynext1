d8eda06d056436cfaf127037e168a9bb64dd8a7f
andrew <andrew_mokryj@ukr.net>
2020-06-24 23:09:06 +0300

    change switches

--- lib/screen/navigation_settings.dart
+++ lib/screen/navigation_settings.dart
@@ -8,9 +8,35 @@ class NavigationSettingsScreen extends StatefulWidget {
 }
 
 class _NavigationSettingsScreenState extends State<NavigationSettingsScreen> {
+  List<Map> mapList;
+
+  @override
+  void initState() {
+    mapList = [
+      {
+        "name": "Apple Maps",
+        "installed": true,
+        "status": true,
+      },
+      {
+        "name": "Google Maps",
+        "installed": true,
+        "status": false,
+      },
+      {
+        "name": "Waze",
+        "installed": false,
+        "status": false,
+      }
+    ];
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final mapList = ["Apple Maps", "Google Maps", "Waze"];
+//    final mapList = ["Apple Maps", "Google Maps", "Waze"];
+//    final statusList = [, "Google Maps", "Waze"];
 
     return Scaffold(
       appBar: AppBar(
@@ -19,14 +45,21 @@ class _NavigationSettingsScreenState extends State<NavigationSettingsScreen> {
         actions: <Widget>[const SizedBox(width: 50)],
       ),
       body: Column(
-        children: mapList.map((e) => ListTile(
-          title: Text(e),
-          subtitle: Text("You don't have $e installed"),
-          trailing: Switch(
-            value: false,
-//            onChanged: (val) => ,
-          ),
-        )).toList(),
+        children: mapList.map((e) =>
+          ListTile(
+            title: Text(e['name']),
+            subtitle: e['installed'] ? null : Text(
+              "You don't have ${e['name']} installed"),
+            trailing: Switch(
+              value: e['status'],
+              onChanged: e['installed'] ? (val) {
+                e['status'] = !(e['status']);
+                setState(() {
+                  mapList = mapList;
+                });
+              } : null,
+            ),
+          )).toList(),
       ),
     );
   }

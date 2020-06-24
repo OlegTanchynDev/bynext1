a7b7a9156a9f2079d30b013d81df4bfb86e9d2f6
andrew <andrew_mokryj@ukr.net>
2020-06-24 22:53:15 +0300

    list draft

--- lib/screen/navigation_settings.dart
+++ lib/screen/navigation_settings.dart
@@ -1,15 +1,32 @@
 import 'package:bynextcourier/view/app_bar_logo.dart';
 import 'package:flutter/material.dart';
 
-class NavigationSettingsScreen extends StatelessWidget {
+class NavigationSettingsScreen extends StatefulWidget {
 
   @override
+  _NavigationSettingsScreenState createState() => _NavigationSettingsScreenState();
+}
+
+class _NavigationSettingsScreenState extends State<NavigationSettingsScreen> {
+  @override
   Widget build(BuildContext context) {
+    final mapList = ["Apple Maps", "Google Maps", "Waze"];
+
     return Scaffold(
       appBar: AppBar(
         title: AppBarLogo(),
         centerTitle: true,
         actions: <Widget>[const SizedBox(width: 50)],
+      ),
+      body: Column(
+        children: mapList.map((e) => ListTile(
+          title: Text(e),
+          subtitle: Text("You don't have $e installed"),
+          trailing: Switch(
+            value: false,
+//            onChanged: (val) => ,
+          ),
+        )).toList(),
       ),
     );
   }

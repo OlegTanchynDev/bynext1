527e2e83eeb95d8712f2dc0f0d19f349186545eb
OlegTanchynDev <o.tanchyn@outlook.com>
2020-06-25 10:24:00 +0300

    move padding inside scroll widget

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -24,115 +24,113 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 23.0, vertical: 8.0),
-        child: LayoutBuilder(builder: (context, constraint) {
-          return SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: constraint.maxHeight,
-              ),
-              child: IntrinsicHeight(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    const SizedBox(height: 8),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                        height: 70,
-                        child: Image(image: AssetImage('assets/images/logo.png')),
-                      ),
+      child: LayoutBuilder(builder: (context, constraint) {
+        return SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 23.0, vertical: 8.0),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: constraint.maxHeight - 8.0 * 2,
+            ),
+            child: IntrinsicHeight(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const SizedBox(height: 8),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      height: 70,
+                      child: Image(image: AssetImage('assets/images/logo.png')),
                     ),
-                    const SizedBox(height: 14),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Image(
-                        image: AssetImage('assets/images/logotype.png'),
-                      ),
+                  ),
+                  const SizedBox(height: 14),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Image(
+                      image: AssetImage('assets/images/logotype.png'),
                     ),
-                    const SizedBox(height: 14),
-                    BlocListener(
-                      bloc: BlocProvider.of<LoginFormBloc>(context),
-                      listener: (context, state) async {
-                        if (state is LoginFormReady && state.error != null && state.error['non_field_errors'] != null) {
-                          await showCustomDialog(
-                            context,
-                            message: state.error['non_field_errors'],
-                            button: FlatButton(
-                              child: Text('OK'),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                          );
-                        }
-                      },
-                      child: BlocBuilder<LoginFormBloc, LoginFormState>(builder: (context, state) {
-                        return Column(
-                          children: <Widget>[
-                            TextFormField(
-                              controller: _usernameController,
-                              decoration: InputDecoration(
-                                hintText: 'Email',
-                                floatingLabelBehavior: FloatingLabelBehavior.never,
-                                errorText:
-                                    state is LoginFormReady && state.error != null ? state.error['username'] : null,
-                              ),
-                              keyboardType: TextInputType.emailAddress,
-                            ),
-                            const SizedBox(
-                              height: 20.0,
-                            ),
-                            TextFormField(
-                              controller: _passwordController,
-                              obscureText: true,
-                              decoration: InputDecoration(
-                                hintText: 'Password',
-                                floatingLabelBehavior: FloatingLabelBehavior.never,
-                                errorText:
-                                    state is LoginFormReady && state.error != null ? state.error['password'] : null,
-                              ),
-                            ),
-                          ],
+                  ),
+                  const SizedBox(height: 14),
+                  BlocListener(
+                    bloc: BlocProvider.of<LoginFormBloc>(context),
+                    listener: (context, state) async {
+                      if (state is LoginFormReady && state.error != null && state.error['non_field_errors'] != null) {
+                        await showCustomDialog(
+                          context,
+                          message: state.error['non_field_errors'],
+                          button: FlatButton(
+                            child: Text('OK'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
                         );
-                      }),
+                      }
+                    },
+                    child: BlocBuilder<LoginFormBloc, LoginFormState>(builder: (context, state) {
+                      return Column(
+                        children: <Widget>[
+                          TextFormField(
+                            controller: _usernameController,
+                            decoration: InputDecoration(
+                              hintText: 'Email',
+                              floatingLabelBehavior: FloatingLabelBehavior.never,
+                              errorText:
+                                  state is LoginFormReady && state.error != null ? state.error['username'] : null,
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          TextFormField(
+                            controller: _passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              floatingLabelBehavior: FloatingLabelBehavior.never,
+                              errorText:
+                                  state is LoginFormReady && state.error != null ? state.error['password'] : null,
+                            ),
+                          ),
+                        ],
+                      );
+                    }),
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: GestureDetector(
+                      child: Text('Forgot your password?'),
+                      onTap: () {
+                        Navigator.of(context).pushNamed(forgotPasswordRoute);
+                      },
                     ),
-                    const SizedBox(
-                      height: 16.0,
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      height: 0,
                     ),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: GestureDetector(
-                        child: Text('Forgot your password?'),
-                        onTap: () {
-                          Navigator.of(context).pushNamed(forgotPasswordRoute);
-                        },
-                      ),
-                    ),
-                    Expanded(
-                      child: SizedBox(
-                        height: 0,
-                      ),
-                    ),
-                    RaisedButton(
-                      child: Text('LOGIN'),
-                      onPressed: () => context
-                          .bloc<LoginFormBloc>()
-                          .submit(_usernameController.text.trim(), _passwordController.text),
-                    ),
-                    RaisedButton(
-                      child: Text('DEMO MODE'),
-                      onPressed: () => context.bloc<LoginFormBloc>().submit('demo', 'demo'),
-                    ),
-                  ],
-                ),
+                  ),
+                  RaisedButton(
+                    child: Text('LOGIN'),
+                    onPressed: () => context
+                        .bloc<LoginFormBloc>()
+                        .submit(_usernameController.text.trim(), _passwordController.text),
+                  ),
+                  RaisedButton(
+                    child: Text('DEMO MODE'),
+                    onPressed: () => context.bloc<LoginFormBloc>().submit('demo', 'demo'),
+                  ),
+                ],
               ),
             ),
-          );
-        }),
-      ),
+          ),
+        );
+      }),
     ));
   }
 }

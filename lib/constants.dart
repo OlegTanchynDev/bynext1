0f9d730e7b1d6281fd21ae51dd09ba8363a0e416
andrew <andrew_mokryj@ukr.net>
2020-07-20 13:59:08 +0300

    change domain to bynext.co

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,10 +4,14 @@ import 'package:flutter/material.dart';
 const googleApiKeyIos = "AIzaSyCZ_S3boVTA-ne7H5PT2NfUBbm_oWygFZU";
 const googleApiKeyAndroid = "AIzaSyBUL46fYVu8TyQm-21H61xd02qOFjqouog";
 
-const servicesUrl = kDebugMode ? "https://playground.cleanlyapp.com/services" : "https://cleanlyapp.com/services";
-const mediaUrl = "https://uploads-static.cleanlyapp.com/";
-const policyUrl = "https://cleanly.com/concierge/ticket-reimbursement-attendance/";
-const generalInfoUrl = "https://cleanly.com/concierge/rating-overview/";
+//const servicesUrl = kDebugMode ? "https://playground.cleanlyapp.com/services" : "https://cleanlyapp.com/services";
+const servicesUrl = kDebugMode ? "https://playground.bynext.co/services" : "https://bynext.co/services";
+//const mediaUrl = "https://uploads-static.cleanlyapp.com/";
+const mediaUrl = "https://uploads-static.bynext.co/";
+//const policyUrl = "https://cleanly.com/concierge/ticket-reimbursement-attendance/";
+const policyUrl = "https://bynext.co/concierge/ticket-reimbursement-attendance/";
+//const generalInfoUrl = "https://cleanly.com/concierge/rating-overview/";
+const generalInfoUrl = "https://bynext.co/concierge/rating-overview/";
 
 const requestTimeout = Duration(seconds: 15);
 
